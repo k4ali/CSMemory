1c6f2fffc4fd646289a6ddda987a0dcfe1ea42d7
v4mmpp <zebientmax007@gmail.com>
2023-08-27 10:34:31 +0400

    feat: add GetProcessHandle method & rename class

---
+++ CSMemory/CSMemory.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CSMemory
+{
+    public class CSMemoryClass
+    {
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, EntryPoint = "OpenProcess")]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags access, bool inheritHandle, int procId);
+
+        public static unsafe IntPtr GetProcessHandle(string processName)
+        {
+            int procId = 0;
+            foreach (var p in Process.GetProcessesByName(processName))
+            {
+                procId = p.Id;
+            }
+
+            IntPtr handle = OpenProcess(ProcessAccessFlags.All, false, procId);
+            return handle;
+        }
+    }
+}
